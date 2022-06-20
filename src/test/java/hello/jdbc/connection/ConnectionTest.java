8ec02adaf66740f0f09e4789029bcba4aa2a6a7e
Kim Juho <sv05159@naver.com>
2022-06-21 00:12:55 +0900

    DataSource 예제2 - 커넥션 풀
    - 10개의 스레드 풀에서 2개를 사용하여 8개가 남는 것을 확인.
    - 11개의 커넥션을 생성하면 예외 발생

--- src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package hello.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -27,6 +28,21 @@ public class ConnectionTest {
         //DriverManagerDataSource - 항상 새로운 커넥션을 획득
         DataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
         useDataSource(dataSource);
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        //커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);  //default
+        dataSource.setPoolName("MyPool");   //풀 이름 지정 (로그 확인용)
+
+        useDataSource(dataSource);
+        Thread.sleep(1000);  //너무 빠르게 처리되어서, 풀을 확인시 사용!
+
     }
 
     private void useDataSource(DataSource dataSource) throws SQLException {
